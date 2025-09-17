c40cb11082c2e1aa98f40aa2e2407faa2384f3ad
Naveenkumaran R <naveenkumaranr2016@gmail.com>
2025-09-17 19:04:19 +0530

    Update index.js
    l

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5176', // your frontend URL
+  origin: 'http://localhost:5173', // your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
